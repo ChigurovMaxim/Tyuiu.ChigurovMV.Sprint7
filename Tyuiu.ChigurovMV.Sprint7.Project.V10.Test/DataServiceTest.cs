95c0e6a5fb441f288b0fe0ca99bdad2e25d1cfa1
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-12-27 08:11:29 +0500

    Test fixed

--- Tyuiu.ChigurovMV.Sprint7.Project.V10.Test/DataServiceTest.cs
+++ Tyuiu.ChigurovMV.Sprint7.Project.V10.Test/DataServiceTest.cs
@@ -1,11 +1,17 @@
+using Tyuiu.ChigurovMV.Sprint7.Project.V10.Lib;
 namespace Tyuiu.ChigurovMV.Sprint7.Project.V10.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetMatrix()
         {
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint7\TestFileData.csv";
+            string[,] waitArray = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+            var resArray = ds.GetMatrix(path);
+            CollectionAssert.AreEqual(waitArray, resArray);
         }
     }
 }
